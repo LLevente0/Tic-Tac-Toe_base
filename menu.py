786ac7446e342a4b4bb2dbd176764ced7ee1424c
Ladányi Levente <ladanyi.levente@diak.szbi-pg.hu>
2025-03-03 09:26:02 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,10 +1,11 @@
 def get_menu_option():
-  '''
-  Should print a menu with the following options:
+    print("""
   1. Human vs Human
   2. Random AI vs Random AI
   3. Human vs Random AI
   4. Human vs Unbeatable AI
+""")
+
 
   The function should return a number between 1-4.
   If the user will enter invalid data (for example 5), than a message will appear
