c413aae251dbf1c8aaafbca5d749c8949eedf8d7
Ladányi Levente <ladanyi.levente@diak.szbi-pg.hu>
2025-03-03 10:33:55 +0100

    Update board.py

--- board.py
+++ board.py
@@ -20,12 +20,10 @@ def display_board(board):
 
 
 def is_board_full(board):
-
     if "." not in board:
         return True
     else:
         return False
-  pass
 
 
 def get_winning_player(board):
